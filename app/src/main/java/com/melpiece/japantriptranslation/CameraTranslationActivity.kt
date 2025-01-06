48c674c06932b6db99a3b0b05a3e3b8555b45c1a
Melpiece <melpiece1@gmail.com>
2025-01-06 16:43:37 +0900

    add Camera preview

--- app/src/main/java/com/melpiece/japantriptranslation/CameraTranslationActivity.kt
+++ app/src/main/java/com/melpiece/japantriptranslation/CameraTranslationActivity.kt
@@ -1,29 +1,82 @@
 package com.melpiece.japantriptranslation
 
+import android.Manifest
 import android.app.Activity
-import android.content.Intent
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.melpiece.japantriptranslation.ui.theme.JapantriptranslationTheme
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class CameraTranslationActivity : ComponentActivity() {
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                setCameraPreview()
+            } else {
+                // Camera permission denied
+            }
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                setCameraPreview()
+            }
+
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun setCameraPreview() {
         setContent {
             JapantriptranslationTheme {
-                CameraTranslationScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraTranslationScreen()
+                }
             }
         }
     }
@@ -31,26 +84,92 @@ class CameraTranslationActivity : ComponentActivity() {
 
 @Composable
 fun CameraTranslationScreen() {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
-    Column(
+    val preview = Preview.Builder().build()
+    val previewView = remember {
+        PreviewView(context)
+    }
+    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+    val imageCapture = remember {
+        ImageCapture.Builder().build()
+    }
+
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview)
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+    Box(
+        contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "카메라 번역 화면",
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Button(
-            onClick = {
-                val activity = context as? Activity
-                activity?.finish()
-            },
-            modifier = Modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+        AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
         ) {
-            Text(text = "뒤로가기")
+            Button(onClick = { captureImage(imageCapture, context) },
+                modifier = Modifier
+                    .weight(1f)) {
+                Text("캡쳐")
+            }
+            Spacer(modifier = Modifier
+                .weight(0.3f))
+            Button(
+                onClick = {
+                    val activity = context as? Activity
+                    activity?.finish()
+                },
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "뒤로가기")
+            }
         }
     }
+
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+            cameraProvider.addListener({
+                continuation.resume(cameraProvider.get())
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
+
+
+private fun captureImage(imageCapture: ImageCapture, context: Context) {
+    val name = "CameraxImage.jpeg"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        }
+    }
+    val outputOptions = ImageCapture.OutputFileOptions
+        .Builder(
+            context.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+        .build()
+    imageCapture.takePicture(
+        outputOptions,
+        ContextCompat.getMainExecutor(context),
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                println("Successs")
+            }
+            override fun onError(exception: ImageCaptureException) {
+                println("Failed $exception")
+            }
+        })
 }
