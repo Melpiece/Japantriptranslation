f03788b5a65e26adf0f5c8784b85aa53788a4689
Melpiece <melpiece1@gmail.com>
2025-01-07 12:15:23 +0900

    translation chat activity

--- app/src/main/java/com/melpiece/japantriptranslation/VoiceChatActivity.kt
+++ app/src/main/java/com/melpiece/japantriptranslation/VoiceChatActivity.kt
@@ -1,11 +1,15 @@
 package com.melpiece.japantriptranslation
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -18,6 +22,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,6 +33,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
@@ -44,7 +50,6 @@ class VoiceChatActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun VoiceChatScreen() {
     val context = LocalContext.current
@@ -60,7 +65,7 @@ fun VoiceChatScreen() {
     }
     val krLanguage by remember { mutableStateOf(TranslateLanguage.KOREAN) }
     val jpLanguage by remember { mutableStateOf(TranslateLanguage.JAPANESE) }
-    val krJPTranslator = remember(krLanguage, jpLanguage) {
+    val krJpTranslator = remember(krLanguage, jpLanguage) {
         val options = TranslatorOptions.Builder()
             .setSourceLanguage(krLanguage)
             .setTargetLanguage(jpLanguage)
@@ -76,9 +81,92 @@ fun VoiceChatScreen() {
 
         Translation.getClient(options)
     }
+    var isKrJpReady by remember { mutableStateOf(false) }
+    var isJpKrReady by remember { mutableStateOf(false) }
+    LaunchedEffect(krJpTranslator) {
+        var conditions = DownloadConditions.Builder()
+//            .requireWifi()
+            .build()
+        krJpTranslator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                isKrJpReady = true
+            }
+    }
+    LaunchedEffect(jpKrTranslator) {
+        var conditions = DownloadConditions.Builder()
+//            .requireWifi()
+            .build()
+        jpKrTranslator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                isJpKrReady = true
+            }
+    }
 
     var krtext by remember { mutableStateOf("") }
     var jptext by remember { mutableStateOf("") }
+    val krSpeechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val spokenText =
+                result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0) ?: ""
+            krtext = spokenText
+
+            if (isKrJpReady) {
+                krJpTranslator.translate(spokenText)
+                    .addOnSuccessListener { translatedText ->
+                        jptext = translatedText
+                        tts.speak(translatedText, TextToSpeech.QUEUE_FLUSH, null, null)
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(context, "번역 실패", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+    val jpSpeechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val spokenText =
+                result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0) ?: ""
+            jptext = spokenText
+
+            if (isJpKrReady) {
+                jpKrTranslator.translate(spokenText)
+                    .addOnSuccessListener { translatedText ->
+                        krtext = translatedText
+                        tts.speak(translatedText, TextToSpeech.QUEUE_FLUSH, null, null)
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(context, "번역 실패", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+
+    fun startKrSpeechRecognition() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR") // Set language to Korean
+        }
+        krSpeechRecognizerLauncher.launch(intent)
+    }
+
+    fun startJpSpeechRecognition() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ja-JP") // Set language to Japanese
+        }
+        jpSpeechRecognizerLauncher.launch(intent)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -99,6 +187,12 @@ fun VoiceChatScreen() {
                 modifier = Modifier
                     .fillMaxWidth(),
             )
+            Button(
+                onClick = { startJpSpeechRecognition() },
+                modifier = Modifier
+            ) {
+                Text(text = "日本語 音声認識")
+            }
         }
         Column(
             modifier = Modifier
@@ -111,21 +205,29 @@ fun VoiceChatScreen() {
                 modifier = Modifier
                     .fillMaxWidth(),
             )
+            Button(
+                onClick = { startKrSpeechRecognition() },
+                modifier = Modifier
+            ) {
+                Text(text = "한국어 음성 인식")
+            }
         }
-        Text(
-            text = "음성 대화 화면",
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-
         Button(
             onClick = {
                 val activity = context as? Activity
                 activity?.finish()
             },
-            modifier = Modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         ) {
             Text(text = "뒤로가기")
         }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun VoiceChatScreenPreview() {
+    JapantriptranslationTheme {
+        VoiceChatScreen()
     }
 }
