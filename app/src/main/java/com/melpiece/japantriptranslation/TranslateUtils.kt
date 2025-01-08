643a7f9bad5c4ee2233b0fb63d8d6f5aa8786484
Melpiece <melpiece1@gmail.com>
2025-01-08 15:41:07 +0900

    back icon refactoring

--- app/src/main/java/com/melpiece/japantriptranslation/TranslateUtils.kt
+++ app/src/main/java/com/melpiece/japantriptranslation/TranslateUtils.kt
@@ -1,5 +1,16 @@
 package com.melpiece.japantriptranslation
 
+import android.app.Activity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
@@ -11,4 +22,20 @@ fun createTransClient(source: String, target: String): Translator {
         .build()
 
     return Translation.getClient(options)
+}
+@Composable
+fun BackIcon(){
+    val context = LocalContext.current
+    Icon(
+        painter = painterResource(R.drawable.back),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable {
+                val activity = context as? Activity
+                activity?.finish()
+            }
+            .size(30.dp),
+        tint = Color.Unspecified
+    )
 }
